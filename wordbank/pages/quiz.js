ce905b466895d79ae1bcbde22e2e957b5b386ca8
Parker S <111610335+Psenter@users.noreply.github.com>
2023-11-22 16:25:10 -0500

    Fixing bootstrap issues.

--- wordbank/pages/quiz.js
+++ wordbank/pages/quiz.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import styles from "../styles/styles.css";
 import Navbar from "../components/navbar";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 //holds all react words
 export const reactWords = {
--- wordbank/pages/wordBank.js
+++ wordbank/pages/wordBank.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "../styles/styles.css";
 import Navbar from "../components/navbar";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 //holds all react words
 export const reactWords = {
