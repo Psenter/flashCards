5816e4001998a116078a3b9c0b23510114c94eed
Parker S <111610335+Psenter@users.noreply.github.com>
2023-11-22 16:20:45 -0500

    Fixing bootstrap issues.

--- wordbank/src/app/page.js
+++ wordbank/src/app/page.js
@@ -1,11 +1,7 @@
 import styles from '../../styles/styles.css'
 import WordBank from '../../pages/wordBank'
-import 'bootstrap/dist/css/bootstrap.css'
 
 export default function Home() {
-  useEffect(() => {
-    import("bootstrap/dist/js/bootstrap");
-  }, []);
   return (
     <div>
       <WordBank />
