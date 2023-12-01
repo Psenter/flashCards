317454742a689a768bf331990a5ee3d977f0df72
Parker S <111610335+Psenter@users.noreply.github.com>
2023-12-01 00:32:38 -0500

    Fixing bootstrap.

--- wordbank/src/app/page.js
+++ wordbank/src/app/page.js
@@ -1,15 +1,14 @@
-import React from 'react'
-import styles from '../../styles/styles.css'
-import WordBank from '../../pages/wordBank'
+import React from "react";
+import styles from "../../styles/styles.css";
+import WordBank from "../../pages/wordBank";
+import Script from "next/script";
 
 export default function Home() {
   return (
-    <html lang="en">
-      <body>
-        <WordBank />
-        <Script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></Script>
-        <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></Script>
-      </body>
-    </html>
-  )
+    <div>
+      <WordBank />
+      <Script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></Script>
+      <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></Script>
+    </div>
+  );
 }
